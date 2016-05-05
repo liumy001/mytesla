8761508c3239cab5856ee5acd50e96c1a1e97ee6
liumingyue0203 <liumingyue0203@163.com>
2016-05-05 11:18:25 +0800

    Initial commit

---
+++ src/main/java/com/ziroom/mytesla/business/room/controller/RoomResource.java
@@ -0,0 +1,35 @@
+package com.ziroom.mytesla.business.room.controller;
+
+
+
+import javax.annotation.Resource;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.ziroom.mytesla.business.room.model.Room;
+import com.ziroom.mytesla.business.room.service.IRoomService;
+
+/**
+ * This bean is default(singleton) scope
+ */
+@Component
+@Path("/matsl")
+@Scope("request")
+public class RoomResource {
+	@Resource
+	private IRoomService roomService;
+	
+    @GET
+    @Produces({"text/plain"})
+    @Path("/room")
+    public String testTesla(Integer roomId) {
+        roomId=2;
+    	Room room = roomService.query(roomId);
+    	return "我是房间";
+    }
+}
