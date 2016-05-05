eb41ab14394d6e18748e3e5d05bb43cd9b6f9e10
liumingyue0203 <liumingyue0203@163.com>
2016-05-05 11:23:35 +0800

    liumy

--- src/main/java/com/ziroom/mytesla/business/room/controller/indexController.java
+++ src/main/java/com/ziroom/mytesla/business/room/controller/indexController.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("index")
 public class indexController {
 
+
+
+
+
     @RequestMapping("index")
     public String index() {
         return "index/index";
